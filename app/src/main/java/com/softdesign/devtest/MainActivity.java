85a2dd2ac43e0addd37244e5f5611e15ba685ccb
Vladimir Trusov <trusov.public@gmail.com>
2016-06-23 12:59:42 +0300

    Добавлен обработчик нажатия кнопки

--- app/src/main/java/com/softdesign/devtest/MainActivity.java
+++ app/src/main/java/com/softdesign/devtest/MainActivity.java
@@ -2,6 +2,10 @@ package com.softdesign.devtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button msgButton = (Button) findViewById(R.id.message_btn);
+        msgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Нажата кнопка", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
